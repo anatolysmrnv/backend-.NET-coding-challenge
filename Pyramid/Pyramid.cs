14e26285358cdb5c7267ccd7cb7c41571b7e5965
Anatoly <anatolysmrnv@gmail.com>
2022-12-18 23:22:01 +0100

    Rewrote the algorithm through recursion

--- Pyramid/Pyramid.cs
+++ Pyramid/Pyramid.cs
@@ -16,25 +16,42 @@ namespace Pyramid
 {
     public class Program
     {
-        private static void Pyramid(int height)
+        private static void Pyramid(int height, int row = 0)
         {
-            for (int row = 1; row <= height; row++)
+            // Base case: If we have reached the last row, print it and return
+            if (row == height)
             {
-                // Print the leading spaces
-                for (int i = 0; i < height - row; i++)
-                {
-                    Console.Write(" ");
-                }
-
-                // Print the asterisks
-                for (int i = 0; i < 2 * row - 1; i++)
-                {
-                    Console.Write("*");
-                }
-
-                // Move to the next line
-                Console.WriteLine();
+                PrintRow(height, row);
+                return;
             }
+
+            // Print the current row
+            PrintRow(height, row);
+
+            // Recurse to the next row
+            Pyramid(height, row + 1);
+        }
+
+        private static void PrintRow(int height, int row)
+        {
+            // Calculate the number of spaces and asterisks to print
+            int spaces = height - row - 1;
+            int asterisks = 2 * row + 1;
+
+            // Print the leading spaces
+            for (int i = 0; i < spaces; i++)
+            {
+                Console.Write(" ");
+            }
+
+            // Print the asterisks
+            for (int i = 0; i < asterisks; i++)
+            {
+                Console.Write("*");
+            }
+
+            // Move to the next line
+            Console.WriteLine();
         }
 
         public static void Main(string[] args)
